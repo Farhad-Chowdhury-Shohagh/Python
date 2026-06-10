940c55da2ce537a2856662337766893e73fd6ccc
Farhad Chowdhury <166280132+Farhad-Chowdhury-Shohagh@users.noreply.github.com>
2026-06-10 23:34:51 +0600

    Add files via upload

---
+++ rectangleArea.py
@@ -0,0 +1,4 @@
+l = float(input("Enter the length of the rectangle(cm): "))
+w = float(input("Enter the width of the rectangle(cm): "))
+area = l * w
+print(f"The area of the rectangle is: {area:.2f} cm²")
